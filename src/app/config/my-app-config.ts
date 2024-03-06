152da67cd6118540a49f1b4fee50a244900552d1
Gregory Danquoins <danquoinsgregory@gmail.com>
2024-03-06 15:54:43 +0100

    fix git problem

--- src/app/config/my-app-config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  oidc: {
-    clientId: '0oaff83hpbLBN6Ted5d7',
-    issuer: 'https://dev-63764937.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:4200/login/callback',
-    scopes: ['openid', 'profile', 'email'],
-  },
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  luv2shopApiUrl: 'https://localhost:8443/api',
-};
